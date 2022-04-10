ce2198facae5ee8e6f3534d1c9e3959f687f7294
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-10 16:40:47 +0800

    删除intersectionfilterfun

--- __tests__/main-test-functions-1.ts
+++ __tests__/main-test-functions-1.ts
@@ -1,9 +1,7 @@
 import "core-js/actual/array/at";
 
-import { testcheckcurrentsegmentsintersectnextsegment } from "../test/testcheckcurrentsegmentsintersectnextsegment";
 import { testclosedtotalpathlength } from "../test/testclosedtotalpathlength";
 
 it("main test functions-1", () => {
-    testcheckcurrentsegmentsintersectnextsegment();
     testclosedtotalpathlength();
 });
