454f0ea4f9e85077ebe95c0f149b79b280fbb087
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-13 15:55:43 +0800

    Update get_options_of_random_greedy_of_tsp.ts

--- src/get_options_of_random_greedy_of_tsp.ts
+++ src/get_options_of_random_greedy_of_tsp.ts
@@ -1,7 +1,7 @@
 import { ECBasicOption } from "echarts/types/dist/shared";
 import { NodeCoordinates } from "../functions/NodeCoordinates";
 import { cacheble_greed_random_route } from "./cacheble_greed_random_route";
-import { drawrouteofnode_coordinates } from "./drawrouteofnode_coordinates";
+// import { drawrouteofnode_coordinates } from "./drawrouteofnode_coordinates";
 import { get_options_route_of_node_coordinates } from "./get_options_route_of_node_coordinates";
 
 export async function get_options_of_random_greedy_of_tsp({
