4ddbb7085272fc927cb19f7f543296ffcb9f0789
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-16 15:19:40 +0800

    Update update_convergence_coefficient.ts

--- functions/update_convergence_coefficient.ts
+++ functions/update_convergence_coefficient.ts
@@ -1,5 +1,5 @@
 import { max_number_of_stagnation } from "./max_number_of_stagnation";
-
+const convergence_coefficient_grow_speed = 1.25;
 export function update_convergence_coefficient({
     number_of_stagnation,
     coefficient_of_diversity_increase,
@@ -17,7 +17,10 @@ export function update_convergence_coefficient({
         return Math.max(
             1,
             convergence_coefficient /
-                Math.pow(1.25, max_number_of_stagnation) /
+                Math.pow(
+                    convergence_coefficient_grow_speed,
+                    max_number_of_stagnation
+                ) /
                 2
         );
     }
@@ -31,10 +34,10 @@ export function update_convergence_coefficient({
         }
         return convergence_coefficient;
     } else if (iterate_best_length > greedy_length) {
-        convergence_coefficient *= 1.7;
+        convergence_coefficient *= convergence_coefficient_grow_speed ** 3;
         return convergence_coefficient;
     } else {
-        convergence_coefficient *= 1.25;
+        convergence_coefficient *= convergence_coefficient_grow_speed;
         return convergence_coefficient;
     }
 
