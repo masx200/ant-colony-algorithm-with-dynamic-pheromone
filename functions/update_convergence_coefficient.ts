85d02666de19c62bedc8391971e88a7a89c6cb4a
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-16 15:34:45 +0800

    convergence_coefficient_max

--- functions/update_convergence_coefficient.ts
+++ functions/update_convergence_coefficient.ts
@@ -1,5 +1,7 @@
 import { max_number_of_stagnation } from "./max_number_of_stagnation";
 const convergence_coefficient_grow_speed = 1.25;
+const convergence_coefficient_min = 1;
+const convergence_coefficient_max = 1000;
 export function update_convergence_coefficient({
     number_of_stagnation,
     coefficient_of_diversity_increase,
@@ -15,30 +17,28 @@ export function update_convergence_coefficient({
 }): number {
     if (number_of_stagnation > max_number_of_stagnation) {
         return Math.max(
-            1,
+            convergence_coefficient_min,
             convergence_coefficient /
                 Math.pow(
                     convergence_coefficient_grow_speed,
-                    max_number_of_stagnation
-                ) /
-                2
+                    max_number_of_stagnation * 1.5
+                )
         );
     }
     if (coefficient_of_diversity_increase > 0) {
-        convergence_coefficient =
+        convergence_coefficient = Math.max(
+            convergence_coefficient_min,
             convergence_coefficient *
-            Math.pow(1 - coefficient_of_diversity_increase, 2);
-        if (convergence_coefficient < 1) {
-            convergence_coefficient = 1;
-            return convergence_coefficient;
-        }
+                Math.pow(1 - coefficient_of_diversity_increase, 2)
+        );
+
         return convergence_coefficient;
     } else if (iterate_best_length > greedy_length) {
-        convergence_coefficient *= convergence_coefficient_grow_speed ** 3;
-        return convergence_coefficient;
+        convergence_coefficient *= convergence_coefficient_grow_speed ** 2;
+        return Math.min(convergence_coefficient_max, convergence_coefficient);
     } else {
         convergence_coefficient *= convergence_coefficient_grow_speed;
-        return convergence_coefficient;
+        return Math.min(convergence_coefficient_max, convergence_coefficient);
     }
 
     // return convergence_coefficient;
