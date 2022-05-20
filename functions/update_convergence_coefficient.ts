61d71a752a5d95f4791dc5052f7a606b719e3940
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-20 16:38:07 +0800

    Update update_convergence_coefficient.ts

--- functions/update_convergence_coefficient.ts
+++ functions/update_convergence_coefficient.ts
@@ -19,7 +19,7 @@ export function update_convergence_coefficient({
         convergence_coefficient = Math.max(
             convergence_coefficient_min,
             convergence_coefficient *
-                Math.pow(1 - coefficient_of_diversity_increase, 0.5)
+                Math.pow(1 - coefficient_of_diversity_increase, 0.8)
         );
 
         return convergence_coefficient;
