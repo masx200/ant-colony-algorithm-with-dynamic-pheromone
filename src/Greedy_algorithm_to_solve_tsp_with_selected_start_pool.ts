fcc85c951841e186a9990fbe59aca7adceaa59e2
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-23 14:31:58 +0800

    new Worker(                     new URL

--- src/Greedy_algorithm_to_solve_tsp_with_selected_start_pool.ts
+++ src/Greedy_algorithm_to_solve_tsp_with_selected_start_pool.ts
@@ -2,7 +2,7 @@ import { worker_error_listener } from "../functions/worker_error_listener";
 import { createThreadPool } from "./createThreadPool";
 import { create_Worker_comlink } from "./create_Worker_comlink";
 import { GreedyWorkerAPI } from "./GreedyWorkerAPI";
-import Greedy_algorithm_to_solve_tsp_with_selected_start_Worker from "./Greedy_algorithm_to_solve_tsp_with_selected_start.worker?worker&inline";
+// import Greedy_algorithm_to_solve_tsp_with_selected_start_Worker from "./Greedy_algorithm_to_solve_tsp_with_selected_start.worker?worker&inline";
 
 export const Greedy_algorithm_to_solve_tsp_with_selected_start_pool =
     createThreadPool({
@@ -13,8 +13,15 @@ export const Greedy_algorithm_to_solve_tsp_with_selected_start_pool =
         minThreads: 1,
         create: () => {
             return create_Worker_comlink<GreedyWorkerAPI>(() => {
-                const w =
-                    new Greedy_algorithm_to_solve_tsp_with_selected_start_Worker();
+                const w = new Worker(
+                    new URL(
+                        "./Greedy_algorithm_to_solve_tsp_with_selected_start.worker.ts",
+                        import.meta.url
+                    ),
+                    {
+                        type: "module",
+                    }
+                );
 
                 return w;
             }, worker_error_listener);
