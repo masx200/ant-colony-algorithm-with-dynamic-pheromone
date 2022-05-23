7777683fdaf682aceae2a652ee05a4c62e68cb3f
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-23 10:38:37 +0800

    Update update_last_random_selection_probability.ts

--- functions/update_last_random_selection_probability.ts
+++ functions/update_last_random_selection_probability.ts
@@ -7,7 +7,7 @@ export function update_last_random_selection_probability({
     lastrandom_selection_probability: number;
 }): number {
     const nextrandom_selection_probability =
-        coefficient_of_diversity_increase / 50;
+        coefficient_of_diversity_increase / 20;
     lastrandom_selection_probability = Math.max(
         nextrandom_selection_probability,
         lastrandom_selection_probability / 10
