14eca586e23712c3e0e248e3eae5a86db37f4bb7
masx200 <masx200@qq.com>
2022-11-08 16:40:06 +0800

    comlink

--- src/create_TSP_Worker_comlink.ts
+++ src/create_TSP_Worker_comlink.ts
@@ -1,13 +1,7 @@
-// import * as comlink from "comlink";
-// import { DataOfBestChange } from "../functions/DataOfBestChange";
-// import { DataOfFinishGreedyIteration } from "../functions/DataOfFinishGreedyIteration";
-// import { DataOfFinishOneIteration } from "../functions/DataOfFinishOneIteration";
-// import { DataOfFinishOneRoute } from "../functions/DataOfFinishOneRoute";
-// import { DataOfTotal } from "../functions/DataOfTotal";
 import { worker_error_listener } from "../functions/worker_error_listener";
 import { create_Worker_comlink } from "./create_Worker_comlink";
 import { TSPRunnerOptions } from "./TSPRunnerOptions";
-// import TSPWorker from "./TSP_Runner.Worker?worker&inline";
+
 import { TSP_Worker_API } from "./TSP_Worker_API";
 import { TSP_Worker_Remote } from "./TSP_Worker_Remote";
 
@@ -29,41 +23,7 @@ export async function create_TSP_Worker_comlink(
         return w;
     }, worker_error_listener);
     await runner.init_runner(options);
-    // const on_finish_one_iteration = async (
-    //     callback: (data: DataOfFinishOneIteration) => void
-    // ) => {
-    //     runner.on_finish_one_iteration(comlink.proxy(callback));
-    // };
-    // const on_finish_one_route = async (
-    //     callback: (data: DataOfFinishOneRoute) => void
-    // ) => {
-    //     runner.on_finish_one_route(comlink.proxy(callback));
-    // };
-    // const on_best_change = async (
-    //     callback: (data: DataOfBestChange) => void
-    // ) => {
-    //     runner.on_best_change(comlink.proxy(callback));
-    // };
-    // const on_finish_greedy_iteration = async (
-    //     callback: (data: DataOfFinishGreedyIteration) => void
-    // ) => {
-    //     runner.on_finish_greedy_iteration(comlink.proxy(callback));
-    // };
-    // const on_total_change: (
-    //     callback: (data: DataOfTotal) => void
-    // ) => Promise<void> = async (callback) => {
-    //     runner.on_total_change(comlink.proxy(callback));
-    // };
-    const remote = Object.create(runner, {
-        // on_finish_greedy_iteration: { value: on_finish_greedy_iteration },
-        // on_finish_one_route: { value: on_finish_one_route },
-        // on_finish_one_iteration: { value: on_finish_one_iteration },
-        // on_best_change: { value: on_best_change },
-        // on_total_change: { value: on_total_change },
-    }) as typeof runner & {
-        // on_finish_one_route: typeof on_finish_one_route;
-        // on_finish_one_iteration: typeof on_finish_one_iteration;
-        // on_best_change: typeof on_best_change;
-    };
+
+    const remote = Object.create(runner, {}) as typeof runner & {};
     return { remote, worker, terminate };
 }
