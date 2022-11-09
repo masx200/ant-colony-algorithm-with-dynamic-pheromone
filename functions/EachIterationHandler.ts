8467b9164aa4f39d3fb10ce49d5cd4dafa9eca3d
masx200 <masx200@qq.com>
2022-11-09 09:25:56 +0800

    local_optimization_routes

--- functions/EachIterationHandler.ts
+++ functions/EachIterationHandler.ts
@@ -1,4 +1,4 @@
-import { sum, uniqBy } from "lodash";
+import { uniqBy } from "lodash";
 import { assert_true } from "../test/assert_true";
 import { calc_population_relative_information_entropy } from "./calc_population-relative-information-entropy";
 import { get_best_route_Of_Series_routes_and_lengths } from "./get_best_route_Of_Series_routes_and_lengths";
@@ -21,7 +21,6 @@ export async function EachIterationHandler(
 ): Promise<{
     iterate_best_length: number;
     coefficient_of_diversity_increase: number;
-    // nextrandom_selection_probability: number;
 
     optimal_length_of_iteration: number;
     optimal_route_of_iteration: number[];
@@ -54,7 +53,6 @@ export async function EachIterationHandler(
     );
 
     assert_true(!Number.isNaN(current_population_relative_information_entropy));
-    // assert_true(!Number.isNaN(nextrandom_selection_probability));
 
     const iterate_best_lengthandroute =
         get_best_route_Of_Series_routes_and_lengths(routes_and_lengths);
@@ -63,7 +61,6 @@ export async function EachIterationHandler(
     const iterate_best_route = iterate_best_lengthandroute.route;
     const endtime_of_process_iteration = Number(new Date());
 
-    /* 对全局最优解和这轮的路径中的长度排名前一半的路径执行三种局部优化方法 */
     const best_half_routes = Array.from(routes_and_lengths)
         .sort((a, b) => a.length - b.length)
         .slice(0, routes_and_lengths.length / 2);
@@ -75,48 +72,27 @@ export async function EachIterationHandler(
         (a) => a.length
     );
 
-    const optimization_results = await Promise.all(
-        need_to_optimization_routes_and_lengths.map((v) =>
-            local_optimization_route_thread({
-                count_of_nodes,
-                max_segments_of_cross_point,
-                distance_round,
-                route: v.route,
-                max_results_of_k_opt,
-                node_coordinates,
-                length: v.length,
-                max_results_of_k_exchange,
-                max_results_of_2_opt,
-            })
-        )
-    );
-    const {
-        route: optimal_route_of_iteration,
-        length: optimal_length_of_iteration,
-        // time_ms: optimal_time_ms,
-    } = get_best_route_Of_Series_routes_and_lengths(optimization_results);
-    const optimal_time_ms = sum(optimization_results.map((v) => v.time_ms));
-    /* await local_optimization_route_thread({
+    const optimization_results = await local_optimization_route_thread({
         count_of_nodes,
         max_segments_of_cross_point,
         distance_round,
-        route: get_best_route(),
         max_results_of_k_opt,
         node_coordinates,
-        length: get_best_length(),
         max_results_of_k_exchange,
         max_results_of_2_opt,
-    }); */
+        routes_and_lengths: need_to_optimization_routes_and_lengths,
+    });
+
+    const optimal_route_of_iteration = optimization_results.route;
+    const optimal_length_of_iteration = optimization_results.length;
+    const optimal_time_ms = optimization_results.time_ms;
+
     if (optimal_length_of_iteration < get_best_length()) {
         set_global_best(
             optimal_route_of_iteration,
             optimal_length_of_iteration
         );
-        // set_best_length(length);
-        // set_best_route(route);
     }
-    // const optimal_route_of_iteration = iterate_best_route;
-    // const optimal_length_of_iteration = iterate_best_length;
 
     const timems_of_process_iteration =
         endtime_of_process_iteration -
@@ -129,7 +105,7 @@ export async function EachIterationHandler(
         optimal_route_of_iteration,
         iterate_best_length,
         iterate_best_route,
-        // nextrandom_selection_probability,
+
         population_relative_information_entropy:
             current_population_relative_information_entropy,
     };
--- functions/local_optimization_route_pool.ts
+++ functions/local_optimization_route_pool.ts
@@ -1,8 +1,8 @@
-import { createThreadPool } from "../src/createThreadPool";
 import { create_Worker_comlink } from "../src/create_Worker_comlink";
-// import local_optimization_route_Worker from "./local_optimization_route.worker?worker&inline";
+import { createThreadPool } from "../src/createThreadPool";
 import { local_optimization_route_api } from "./local_optimization_route_api";
 import { worker_error_listener } from "./worker_error_listener";
+
 export const local_optimization_route_pool = createThreadPool({
     minThreads: 1,
     terminate(w) {
