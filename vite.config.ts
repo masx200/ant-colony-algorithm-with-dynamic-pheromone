f78bf84dec7347f77620d6b6a28d3f98ae344c5f
masx200 <masx200@qq.com>
2022-11-09 15:57:05 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,32 @@
-import { babel } from "@rollup/plugin-babel";
-import vuePlugin from "@vitejs/plugin-vue";
-import path from "path";
 import AutoImport from "unplugin-auto-import/vite";
-import ElementPlus from "unplugin-element-plus/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import Components from "unplugin-vue-components/vite";
-import { defineConfig, UserConfigExport } from "vite";
+import ElementPlus from "unplugin-element-plus/vite";
+import path from "path";
 import vpchecker from "vite-plugin-checker";
+import vuePlugin from "@vitejs/plugin-vue";
+import { babel } from "@rollup/plugin-babel";
 import { createHtmlPlugin } from "vite-plugin-html";
+import { defineConfig, PluginOption, UserConfig } from "vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import { resolve } from "path";
 import { VitePWA } from "vite-plugin-pwa";
-//@ts-ignore
+
 const checker = vpchecker;
-// console.log(babel)
-//@ts-ignore
-export default defineConfig(({ mode, command }) => {
-    // console.log(mode, command);
+
+export default defineConfig(({ mode, command }): UserConfig => {
     const isdrop = mode === "production" && command === "build";
-    const config: UserConfigExport = {
+    const config: UserConfig = {
         worker: {
             plugins: [
-                //@ts-ignore
                 babel({
                     babelHelpers: "bundled",
                     exclude: [/node_modules/],
                     extensions: [".ts", ".js"],
                     plugins: [
-                        [
-                            "@babel/plugin-proposal-async-generator-functions",
-                            // { allowAllFormats: true },
-                        ],
+                        ["@babel/plugin-proposal-async-generator-functions"],
                     ],
                 }),
-            ],
+            ] as PluginOption[],
         },
         esbuild: {
             legalComments: "none",
@@ -49,35 +44,29 @@ export default defineConfig(({ mode, command }) => {
                 resolvers: [ElementPlusResolver()],
             }),
             checker({
-                // vueTsc: true,
                 typescript: { root: path.resolve(__dirname) },
             }),
-            // checker({ vueTsc: true }),
 
-            ElementPlus({
-                // options
-            }),
+            ElementPlus({}),
             vuePlugin(),
-            // mode === "production" &&
-            //     command === "build" &&
-            // isdrop &&
+
             babel({
                 babelHelpers: "bundled",
                 sourceMaps: mode !== "production",
                 exclude: [/node_modules/],
                 extensions: [".ts", ".js"],
-                //@ts-ignore
+
                 plugins: [
+                    "@babel/plugin-proposal-async-generator-functions",
                     [
                         "babel-plugin-import",
                         {
                             libraryName: "lodash",
                             libraryDirectory: "",
-                            camel2DashComponentName: false, // default: true
+                            camel2DashComponentName: false,
                         },
                     ],
                     isdrop && "babel-plugin-clean-code",
-                    // "@babel/plugin-syntax-typescript",
                 ].filter(Boolean),
             }),
             createHtmlPlugin({
@@ -91,34 +80,17 @@ export default defineConfig(({ mode, command }) => {
                 registerType: "autoUpdate",
                 workbox: { globPatterns: ["*/*"] },
             }),
-            babel({
-                babelHelpers: "bundled",
-                exclude: [/node_modules/],
-                extensions: [".ts", ".js"],
-                plugins: [
-                    [
-                        "@babel/plugin-proposal-async-generator-functions",
-                        // { allowAllFormats: true },
-                    ],
-                ],
-            }),
-            // getBabelOutputPlugin({ plugins: ["babel-plugin-clean-code"] }),
         ],
         build: {
             rollupOptions: {
                 input: resolve(__dirname, "src", "index.html"),
-                // output: { file: resolve(__dirname, "dist", "index.html") },
             },
             cssCodeSplit: false,
             minify: "esbuild",
             emptyOutDir: true,
             outDir: path.resolve(__dirname, "dist"),
             target: "es2018",
-            // terserOptions: {
-            //     compress: { drop_console: true, drop_debugger: true },
-            // },
         },
     };
     return config;
 });
-import { resolve } from "path";
